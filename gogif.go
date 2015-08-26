a0f8e131262f4d23729c055880d9bbeba05d421a
Lee Witek <neomech@gmail.com>
2015-08-26 09:10:04 +0100

    adding support for pre calulated attribute values for colour mapping

--- gogif.go
+++ gogif.go
@@ -16,21 +16,46 @@ func exitOnError(err error) {
 	}
 }
 
-type ColourMapFunc func(idx uint8) (termbox.Attribute, termbox.Attribute)
-
-func test(idx uint8) (termbox.Attribute, termbox.Attribute) {
-	return termbox.Attribute(idx), termbox.Attribute(idx)
+type AttribVals struct {
+	fg termbox.Attribute
+	bg termbox.Attribute
 }
 
-func renderFrame(g *gif.GIF, framenum int, cmap ColourMapFunc) error {
+type AttribTable [256]AttribVals
+
+type ColourMapFunc func(idx uint8) AttribVals
+
+func test(idx uint8) AttribVals {
+	return AttribVals{fg: termbox.Attribute(idx), bg: termbox.Attribute(idx)}
+}
+
+func mapColours(g *gif.GIF, cmap ColourMapFunc) []AttribTable {
+	var attribs []AttribTable
+
+	for f := 0; f < len(g.Image); f++ {
+
+	}
+
+	return attribs
+}
+
+func renderFrame(g *gif.GIF, framenum int, attribs []AttribTable) error {
 
 	width, height := termbox.Size()
+
+	if width > g.Image[framenum].Rect.Dx() {
+		width = g.Image[framenum].Rect.Dx()
+	}
+
+	if height > g.Image[framenum].Rect.Dy() {
+		height = g.Image[framenum].Rect.Dy()
+	}
 
 	for y := 0; y < height; y++ {
 		lineOffset := g.Image[framenum].Stride * y
 		for x := 0; x < width; x++ {
-			fg, bg := cmap(g.Image[framenum].Pix[x+lineOffset])
-			termbox.SetCell(x, y, ' ', fg, bg)
+			attr := attribs[framenum][x+lineOffset]
+			termbox.SetCell(x, y, ' ', attr.fg, attr.bg)
 		}
 	}
 
@@ -63,8 +88,10 @@ func main() {
 
 	frameNumber := 0
 
+	attribs := mapColours(g, test)
+
 	gc.OnTick = func(gc *GameCore) error {
-		err := renderFrame(g, frameNumber, test)
+		err := renderFrame(g, frameNumber, attribs)
 		frameNumber++
 		if len(g.Image) == frameNumber {
 			frameNumber = 0
